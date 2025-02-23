52024b3e90812e30d329e5eaaeac7771b567f675
ranjam <ryanzam2005@gmail.com>
2025-02-23 16:47:53 +0545

    configure prisma and api route

---
+++ app/api/bookings/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+import prisma from "../../../libs/prismadb"
+
+export async function POST(req: Request) {
+    const { fullname, email, phone, serviceDate, description } = await req.json()
+
+    const booking = await prisma.booking.create({
+        data: { fullname, email, phone, serviceDate: new Date(serviceDate), description }
+    })
+
+    return NextResponse.json(booking, { status: 201 })
+}
